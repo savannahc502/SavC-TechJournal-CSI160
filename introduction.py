ff8b20077f42f0071c7545dbba4b2cb4b5e531d6
Savannah <113316987+savannahc502@users.noreply.github.com>
2023-09-04 15:04:02 -0400

    Add files via upload

---
+++ introduction.py
@@ -0,0 +1,38 @@
+#9/4/23 Savannah Ciak
+#This is a comment, the interpreter will not process it as code
+#To create a block comment, use quote marks as seen below
+
+"""
+Here is a block comment
+It takes up two lines.
+"""
+
+'''
+Here is another block comment
+Using a series of three single quotes
+'''
+
+"""
+Four Basic Data Types: 
+String --> "Hello World", "5"
+Integer --> 5
+Float --> 5.12
+Boolean --> True False, 1 and 0
+
+Variable Names Rules: 
+No Spaces
+Never start with a number
+Do not use special characters
+
+PEP8 Standard!
+"""
+
+#A Variable
+num1 = 2
+num2 = 8 #If this was "8", it would not work because that would be a string, not an integer. 
+
+result = num1 * num2
+result = num2 / num1 #Variable Reassignment
+result2 = num1 / num2
+print(result)
+print(result2)
