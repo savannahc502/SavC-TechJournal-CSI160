000acc81f128af41e37e2c83dc5a91c4e6a62a29
Savannah C <113316987+savannahc502@users.noreply.github.com>
2023-09-25 12:35:17 -0400

    Update Class Exercise 1.py

--- Class Exercise 1.py
+++ Class Exercise 1.py
@@ -6,30 +6,38 @@ Hannelore Sanokklis
 Lily Pouliot
 """
 import math
+
 def start_conversion():
     print("You have the options to convert Binary to Hexadecimal (1), Binary to Base-10 (2), Hexadecimal to Base-10 (3), Hexadecimal to Binary (4), Base-10 to Binary (5), Base-10 to Hexadecimal (6)")
-    conversion_type = input("What converstion do you want? Please enter an integer number between 1-6. Reference the options above.")
-    if(conversion_type == 1):
-        num = bin(int(input("What is the starting number?")))
-        return (Bin_to_Hex(num))
+    conversion_type = int(input("What conversion do you want? Please enter an integer number between 1-6. Reference the options above."))
 
-    elif(conversion_type == 5):
+    if (conversion_type == 1):
+        num = hex(int(input("What is the binary number?")))
+        print(bin_to_hex(num))
+
+    elif (conversion_type == 2):
+        userinput = input("What is the binary number?")
+        num = int(userinput, 2)
+        print(bin_to_base10(num))
+
+    elif (conversion_type == 5):
         num = int(input("What is the starting number?"))
-        return (base10_bin(num))
+        return (base10_to_bin(num))
 
-    """elif(conversion_type == 2):
-    elif(conversion_type == 3):
-    elif(conversion_type == 4):
-    
-    elif(conversion_type == 6):"""
-    num = input("What is the starting number?")
-    #num_type = input("Is this a Binary, Hexadecimal, or Base-10 number?")
+    # num_type = input("Is this a Binary, Hexadecimal, or Base-10 number?")
 
-def Bin_to_Hex(num):
+
+def bin_to_hex(num):  # Option 1
     print(num)
 
-def base10_bin(num):
+
+def bin_to_base10(num):  # Option 2
+    print(num)
+
+
+def base10_to_bin(num):  # Option 5
     num5 = bin(num)
     print(num5)
-    
-print(start_conversion())
+
+
+start_conversion()
