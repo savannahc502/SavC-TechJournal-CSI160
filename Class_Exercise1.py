b725c45d9a64e8db1784f8e11461aea7bc003a5d
Savannah C <113316987+savannahc502@users.noreply.github.com>
2023-09-26 18:45:11 -0400

    Update Class_Exercise1.py

--- Class_Exercise1.py
+++ Class_Exercise1.py
@@ -1,9 +1,19 @@
-"""
-Team Members:
-Adra Gonzalez
-Savannah Ciak
-Hannelore Sanokklis
-Lily Pouliot
+"""This file can convert binary, hexadecimal, and base-10 integer inputs into each other.
+
+Authors: Adra Gonzalez, Savannah Ciak, Hannelore Sanokklis, Lily Pouliot
+Class: CSI-160-01
+Assignment: Class Exercise 1
+Due Date: 21 September 2023
+
+Certification of Authenticity:
+I certify that this is entirely my own work, except where I have given
+fully-documented references to the work of others. I understand the definition
+and consequences of plagiarism and acknowledge that the assessor of this
+assignment may, for the purpose of assessing this assignment:
+- Reproduce this assignment and provide a copy to another member of academic
+- staff; and/or Communicate a copy of this assignment to a plagiarism checking
+- service (which may then retain a copy of this assignment on its database for
+- the purpose of future plagiarism checking)
 """
 
 
