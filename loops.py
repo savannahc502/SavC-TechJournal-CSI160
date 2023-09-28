c1f28cd4169e4a33b4b01bfb7c7f1c88912b44bb
Savannah C <113316987+savannahc502@users.noreply.github.com>
2023-09-28 12:43:57 -0400

    Update and rename Blank to loops.py

---
+++ loops.py
@@ -0,0 +1,24 @@
+# Working with loops
+
+mixedList = ["john", "sally", "sam", "Larry", "Mary", 1, 2, 6, 7, 9, 0, 2, 4, 5]
+title = "Python Programming"
+
+''''
+for i in mixedList:
+    print("The item in this list is:", i)  # Prints a single line for each list item
+'''
+
+'''
+spam = 0
+if (spam < 5):
+    print('Hello, world.')
+    spam = spam + 1
+else:
+    print("You fail if you forget the 'else' statement!")
+'''
+
+# The 'while' loop
+x = 0
+while x < 5:
+    print(x)
+    x = x + 1
