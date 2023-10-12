9014366fefec9fa2a92e299f864c7ff7b39223ab
Savannah C <113316987+savannahc502@users.noreply.github.com>
2023-10-12 12:55:45 -0400

    Add files via upload

---
+++ review_lists.py
@@ -0,0 +1,41 @@
+# Reviewing how to work with lists
+
+numbers = [0, 1, 3, 5, 4, 2, 9, 6]
+numbers.sort()
+print(numbers)
+# x = numbers.median()
+# print(x) #Returns a Traceback
+
+list_sum = sum(numbers)  # Adds all the numbers in the list
+list_min = min(numbers)  # Finds the minimum value in a list
+list_max = max(numbers)  # Finds the maximum value in a list
+
+# print(list_sum)
+# print(list_min)
+# print(list_max)
+
+# A way to caluclate the average of numbers in a list
+# Average is to sum all values and then divide by the number of values in the list
+list_avg = sum(numbers)/len(numbers)
+# print("The average of the numbers in the list equals",list_avg)
+
+letters = ['a', 'w', 'q', 'a', 's', 'd', 'c', 'z', 's']
+# Develop code that will allow a user to input a letter. Search for the letter. Return its index position
+# and how many times it was found in the list. If it is not in the list, let the user know it was not
+# found in the list. Ask if the user would like to add it to the list. If yes, add it. If no, end.
+
+user_letter = input("Please enter a lowercase letter of the English alphabet: ")
+for i in letters:
+    count = 0
+    if user_letter == i:
+        count = count + 1
+        index = letters.index(i)
+        print("The letter", user_letter, "was found at index", enumerate(letters))
+    elif user_letter != i:
+        user_append = input("Letter not found. Would you like to add the letter to the list? (yes/no):")
+        if user_append == "yes":
+            letters.append(user_letter)
+            print("The list is now:", letters)
+        else:
+            print("Letter was not added to the list.")
+
