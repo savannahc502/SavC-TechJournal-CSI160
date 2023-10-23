b6c92caa45c0a4c224efa1ef6956164f30761082
Savannah C <113316987+savannahc502@users.noreply.github.com>
2023-10-23 12:35:47 -0400

    Create reviewfunctions.py

---
+++ reviewfunctions.py
@@ -0,0 +1,29 @@
+# Reviewing how to work with functions
+
+def get_info():
+    name = input("Enter username here: ")
+    pswd = input("Enter your password: ")
+    bank_acct = input("What is your bank account number?")
+    showData(name, pswd, bank_acct)
+
+customer = []
+
+def showData(name, pswd, bank_acct):
+    print("Hello,", name + ".", "Thank you for your information.")
+    print("We are adding your information to our dataset.")
+    customer.append([name, pswd, bank_acct])
+    print("Please confirm we have the correct data.")
+    '''
+        Creates a simple list
+        customer.append(name)
+        customer.append(pswd)
+        customer.append(bank_acct)
+    '''
+    save_customer_datab = tuple(customer)
+    print("Your full name:", name)
+    print("Your password:", pswd)
+    print("Bank account number:", bank_acct)
+    print(customer)
+    print(save_customer_datab)
+
+get_info()
