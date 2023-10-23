0777030a041bbfe8791b17c80e0a55a92ad44db8
Savannah C <113316987+savannahc502@users.noreply.github.com>
2023-10-23 12:03:16 -0400

    Update and rename test1 to strings.py

---
+++ strings.py
@@ -0,0 +1,20 @@
+# Working with Importing Modules 10/23/23
+import math
+from datetime import datetime
+# Wierd import method above, but sometimes that's just how it is.
+# import replit
+# importing replit > file > settings > + > search replit > install package
+p = math.pi
+
+# Working with f_strings 10/23/23
+name = "Jane"
+age = 25
+
+print("Hello, {}! You're {} years old.".format(name, age))
+print("Hello,", name + "! You're", age, "years old.")
+
+# The Modulo Operator, %
+print("Hello, %s!" % name)
+print("Hello,", name + "!")
+
+# https://realpython.com/python-f-strings/
